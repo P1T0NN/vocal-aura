198048354080fb35fe23740bb8eac2cc1b124275
P1T0NN <ognjen.tapuskovic@gmail.com>
2025-02-01 15:14:42 +0100

    Fixed desktop-nav

--- src/components/ui/header/desktop-nav.tsx
+++ src/components/ui/header/desktop-nav.tsx
@@ -1,10 +1,5 @@
-"use client"
-
-// NEXTJS IMPORTS
-import { usePathname, useRouter } from "next/navigation";
-
 // LIBRARIES
-import { motion } from "framer-motion";
+import * as motion from "motion/react-client";
 
 // COMPONENTS
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
@@ -13,24 +8,6 @@ import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuL
 import { menuItems } from "./header-data";
 
 export const DesktopNav = () => {
-    const pathname = usePathname();
-    const router = useRouter();
-
-    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, href: string) => {
-        e.preventDefault();
-        
-        if (pathname === "/") {
-            // On home page, scroll to section
-            const element = document.querySelector(href);
-            if (element) {
-                element.scrollIntoView({ behavior: "smooth" });
-            }
-        } else {
-            // On other pages, navigate to home page with hash
-            router.push(`/${href}`);
-        }
-    }
-
     return (
         <div className="hidden md:block">
             <NavigationMenu className="relative">
@@ -45,7 +22,6 @@ export const DesktopNav = () => {
                                 <NavigationMenuLink
                                     className="group inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-white/10 hover:text-white focus:bg-white/10 focus:text-white focus:outline-none disabled:pointer-events-none disabled:opacity-50 text-gray-100"
                                     href={item.href}
-                                    onClick={(e) => handleClick(e, item.href)}
                                 >
                                     <item.icon className="mr-2 h-4 w-4" />
                                     <span>{item.label}</span>
